f803a7e2fba3d3175b1c5f06534e0d499564f10a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-04-26 21:28:26 +0700

    view barang

--- view/view.barang.pas
+++ view/view.barang.pas
@@ -5,11 +5,24 @@ unit view.barang;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, db, FileUtil, Forms, Controls, Graphics, Dialogs, DBGrids,
+  ExtCtrls, StdCtrls, vm.barang, connection.dm;
 
 type
+
+  { TFrmBarang }
+
   TFrmBarang = class(TForm)
+    BtnRefresh: TButton;
+    DsBarang: TDataSource;
+    DBGrid1: TDBGrid;
+    Panel1: TPanel;
+    procedure BtnRefreshClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
   private
+    FVmBarang: TVmBarang;
+    procedure RefreshBarang;
     { private declarations }
   public
     { public declarations }
@@ -18,6 +31,28 @@ type
 implementation
 
 {$R *.lfm}
+
+{ TFrmBarang }
+
+procedure TFrmBarang.BtnRefreshClick(Sender: TObject);
+begin
+  RefreshBarang;
+end;
+
+procedure TFrmBarang.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  FVmBarang.Free;
+end;
+
+procedure TFrmBarang.FormCreate(Sender: TObject);
+begin
+  FVmBarang := TVmBarang.Create(dm.MyCon);
+end;
+
+procedure TFrmBarang.RefreshBarang;
+begin
+  DsBarang.DataSet := FVmBarang.Data;
+end;
 
 end.
 
